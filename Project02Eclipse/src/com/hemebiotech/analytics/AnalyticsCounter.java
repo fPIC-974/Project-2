80e8697a5d0461873514be4ea76e045cf83b8f36
Frederic Picard <fred.picard@gmail.com>
2022-12-06 13:44:00 +0100

    Adding comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,19 +5,19 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	// initialize to 0 - fPIC : useless comment
+	private static int rashCount = 0;		// initialize to 0 - fPIC : useless comment
+	private static int pupilCount = 0;		// initialize to 0 - fPIC : useless comment
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws Exception { // fPIC : exceptions not used in this class
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int i = 0;	// set i to 0 - fPIC : useless comment
+		int headCount = 0;	// counts headaches - fPIC : difference with headacheCount ?
 		while (line != null) {
-			i++;	// increment i
+			i++;	// increment i - fPIC : useless comment; unused variable
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
@@ -26,18 +26,18 @@ public class AnalyticsCounter {
 			else if (line.equals("rush")) {
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
+			else if (line.contains("pupils")) { // fPIC : testing substring match, could result in inaccuracy in result
 				pupilCount++;
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
 		
-		// next generate output
+		// next generate output - opening, closing not tested for exceptions
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
+		writer.write("headache: " + headacheCount + "\n"); // fPIC : variable not modified in previous code, resulting in original bug
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + pupilCount + "\n"); // fPIC : inacurate result, matches dialated pupils AND constricted pupils
 		writer.close();
 	}
 }
