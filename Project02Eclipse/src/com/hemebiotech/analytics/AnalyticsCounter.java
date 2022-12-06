37ebac2aa48d7ca807160acc0a02b20b3364081a
Frederic Picard <fred.picard@gmail.com>
2022-12-06 14:35:02 +0100

    Debugging original code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,51 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0 - fPIC : useless comment
-	private static int rashCount = 0;		// initialize to 0 - fPIC : useless comment
-	private static int pupilCount = 0;		// initialize to 0 - fPIC : useless comment
-	
-	public static void main(String args[]) throws Exception { // fPIC : exceptions not used in this class
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+
+	public static void main(String[] args) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		BufferedReader reader = null;
+		String line = null;
 
-		int i = 0;	// set i to 0 - fPIC : useless comment
-		int headCount = 0;	// counts headaches - fPIC : difference with headacheCount ?
-		while (line != null) {
-			i++;	// increment i - fPIC : useless comment; unused variable
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) { // fPIC : testing substring match, could result in inaccuracy in result
-				pupilCount++;
-			}
+		try {
+			reader = new BufferedReader(new FileReader("symptoms.txt"));
+			line = reader.readLine();
 
-			line = reader.readLine();	// get another symptom
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+
+				if (line.equals("headache")) {
+					headacheCount++;
+					System.out.println("number of headaches: " + headacheCount);
+				} else if (line.equals("rash")) {
+					rashCount++;
+				} else if (line.equals("dialated pupils")) {
+					pupilCount++;
+				}
+
+				line = reader.readLine(); // get another symptom
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		// next generate output - opening, closing not tested for exceptions
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n"); // fPIC : variable not modified in previous code, resulting in original bug
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n"); // fPIC : inacurate result, matches dialated pupils AND constricted pupils
-		writer.close();
+
+		// next generate output
+		FileWriter writer = null;
+
+		try {
+			writer = new FileWriter("result.out");
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -16,5 +16,5 @@ public interface ISymptomReader {
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms (); // fPIC : method definition does not use Java naming convention getSymptoms
+	List<String> getSymptoms ();
 }
